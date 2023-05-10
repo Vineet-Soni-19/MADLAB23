5dafcbad9d94ac52d001c4273b4c4755753c0a53
Vineet-Soni-19 <100026158+Vineet-Soni-19@users.noreply.github.com>
2023-05-10 10:38:29 +0530

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,8 @@
+public class Application {
+	public static void main (){
+        Mother m=new Mother();
+        m.show();
+        Child ch=new Child ();
+        ch.show ();
+    }
+}
---
+++ Main.java
@@ -0,0 +1,6 @@
+class Main {
+    public static void main(String[] args){
+        Application A=new Application();
+        A.main();
+    }
+}
