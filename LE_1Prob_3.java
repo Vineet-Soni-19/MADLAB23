7689b36103b958bb7680c3d95bd2200ef00fa385
Vineet-Soni-19 <100026158+Vineet-Soni-19@users.noreply.github.com>
2023-05-11 16:08:43 +0530

    Delete LE_1Prob_3.java

--- LE_1Prob_3.java
+++
@@ -1,8 +0,0 @@
-public class Main{
-	public static void main(String[] args) {
-	    int n=Integer.parseInt(args[0]);
-	    for(int i=1; i<=10; i++){
-	        System.out.println(n+" x "+i+" = "+(n*i));
-	    }
-	}
-}
